f02c7f83aca5c53a96ac383bb3da82dbe0fc965e
Birukov Jury <biryukov.y.mi@sberbank.ru>
2019-07-02 16:19:24 +0700

    Gmap configured

--- app/src/main/java/com/petproject/ybiry/galleryonmap/ui/main/MainActivity.java
+++ app/src/main/java/com/petproject/ybiry/galleryonmap/ui/main/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends BaseActivity<ActivityMainBinding>
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
--- app/src/main/java/com/petproject/ybiry/galleryonmap/ui/main/MainFragment.java
+++ app/src/main/java/com/petproject/ybiry/galleryonmap/ui/main/MainFragment.java
@@ -1,10 +1,42 @@
 package com.petproject.ybiry.galleryonmap.ui.main;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.petproject.ybiry.galleryonmap.R;
 import com.petproject.ybiry.galleryonmap.arch.BaseViewModelFragment;
 import com.petproject.ybiry.galleryonmap.databinding.FragmentMainBinding;
 
-public class MainFragment extends BaseViewModelFragment<FragmentMainBinding, MainFragmentViewModel> {
+public class MainFragment extends BaseViewModelFragment<FragmentMainBinding, MainFragmentViewModel>
+        implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMapFragment = (SupportMapFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mMapFragment != null) {
+            mMapFragment.getMapAsync(this);
+        }
+    }
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_main;
@@ -18,5 +50,15 @@ public class MainFragment extends BaseViewModelFragment<FragmentMainBinding, Mai
     @Override
     protected void setSubscribers() {
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
