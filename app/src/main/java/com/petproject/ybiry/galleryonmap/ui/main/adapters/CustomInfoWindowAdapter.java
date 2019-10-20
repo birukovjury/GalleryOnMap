8a2dae8ba0c153c3cf75eceab5a33421535add9f
biryukov-ym <biryukov.y.mi@sberbank.ru>
2019-10-20 13:22:03 +0700

    info adapter improved

--- app/src/main/java/com/petproject/ybiry/galleryonmap/ui/main/adapters/CustomInfoWindowAdapter.java
+++ app/src/main/java/com/petproject/ybiry/galleryonmap/ui/main/adapters/CustomInfoWindowAdapter.java
@@ -1,9 +1,7 @@
 package com.petproject.ybiry.galleryonmap.ui.main.adapters;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,11 +13,11 @@ import com.google.maps.android.clustering.ClusterManager;
 import com.petproject.ybiry.galleryonmap.R;
 import com.petproject.ybiry.galleryonmap.data.model.Photo;
 
-import java.util.Collection;
-
 public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
     private static final String TAG = "infoWindowAdapter";
+    private static final int MAX_WIDTH = 100;
+    private static final int MAX_HEIGHT = 100;
     private final Activity mContext;
     private Cluster<Photo> mCluster;
 
@@ -39,7 +37,6 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
     @Override
     public View getInfoContents(Marker marker) {
         View view = mContext.getLayoutInflater().inflate(R.layout.custom_info_window, null);
-
         TextView titleTextView = view.findViewById(R.id.tv_title);
         ImageView img1 = view.findViewById(R.id.pic1);
         ImageView img2 = view.findViewById(R.id.pic2);
@@ -47,46 +44,52 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
         ImageView img4 = view.findViewById(R.id.pic4);
         setVisibility(false, img1, img2, img3, img4);
 
-        if (mCluster == null) {
-            Bitmap bitmap = BitmapFactory.decodeFile(marker.getSnippet());
+        if (mCluster == null) { //means marker tapped, not cluster
             titleTextView.setText(marker.getTitle());
-            img1.setImageBitmap(bitmap);
+            /*
+            Picasso.get()
+                    .load(new File(marker.getSnippet()))
+                    .resize(MAX_WIDTH, MAX_HEIGHT)
+                    .placeholder(R.drawable.spinner)
+                    .into(img1);
+                    */
+            titleTextView.setText(marker.getTitle());
             setVisibility(true, img1);
+            img1.setImageBitmap(BitmapFactory.decodeFile(marker.getSnippet()));
+
         } else {
-            Collection<Photo> markers = mCluster.getItems();
-            Log.d(TAG, "ClusterSize = " + markers.size());
-            titleTextView.setText(mContext.getApplicationContext().getString(R.string.cluster_title));
-            if (!markers.isEmpty()) {
-                Photo[] array = new Photo[markers.size()];
-                array = markers.toArray(array);
-                switch (markers.size()) {
+            if (!mCluster.getItems().isEmpty()) { //means cluster tapped, not marker
+                Photo[] clusterPhotos = new Photo[mCluster.getItems().size()];
+                clusterPhotos = mCluster.getItems().toArray(clusterPhotos);
+
+                switch (clusterPhotos.length) {
                     case 1:
-                        img1.setImageBitmap(BitmapFactory.decodeFile(array[0].getSnippet()));
+                        img1.setImageBitmap(BitmapFactory.decodeFile(clusterPhotos[0].getSnippet()));
                         setVisibility(true, img1);
                         break;
                     case 2:
-                        img1.setImageBitmap(BitmapFactory.decodeFile(array[0].getSnippet()));
-                        img2.setImageBitmap(BitmapFactory.decodeFile(array[1].getSnippet()));
+                        img1.setImageBitmap(BitmapFactory.decodeFile(clusterPhotos[0].getSnippet()));
+                        img2.setImageBitmap(BitmapFactory.decodeFile(clusterPhotos[1].getSnippet()));
                         setVisibility(true, img1, img2);
                         break;
                     case 3:
-                        img1.setImageBitmap(BitmapFactory.decodeFile(array[0].getSnippet()));
-                        img2.setImageBitmap(BitmapFactory.decodeFile(array[1].getSnippet()));
-                        img3.setImageBitmap(BitmapFactory.decodeFile(array[2].getSnippet()));
+                        img1.setImageBitmap(BitmapFactory.decodeFile(clusterPhotos[0].getSnippet()));
+                        img2.setImageBitmap(BitmapFactory.decodeFile(clusterPhotos[1].getSnippet()));
+                        img3.setImageBitmap(BitmapFactory.decodeFile(clusterPhotos[2].getSnippet()));
                         setVisibility(true, img1, img2, img3);
                         break;
                     case 4:
                     default:
-                        img1.setImageBitmap(BitmapFactory.decodeFile(array[0].getSnippet()));
-                        img2.setImageBitmap(BitmapFactory.decodeFile(array[1].getSnippet()));
-                        img3.setImageBitmap(BitmapFactory.decodeFile(array[2].getSnippet()));
-                        img4.setImageBitmap(BitmapFactory.decodeFile(array[3].getSnippet()));
+                        img1.setImageBitmap(BitmapFactory.decodeFile(clusterPhotos[0].getSnippet()));
+                        img2.setImageBitmap(BitmapFactory.decodeFile(clusterPhotos[1].getSnippet()));
+                        img3.setImageBitmap(BitmapFactory.decodeFile(clusterPhotos[2].getSnippet()));
+                        img4.setImageBitmap(BitmapFactory.decodeFile(clusterPhotos[3].getSnippet()));
                         setVisibility(true, img1, img2, img3, img4);
                 }
+                titleTextView.setText(mContext.getApplicationContext().getString(R.string.cluster_title));
             }
         }
 
-        setMaxHeight(30, img1, img2, img3, img4);
         return view;
     }
 
@@ -98,9 +101,4 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
         }
     }
 
-    private void setMaxHeight(int height, ImageView... views) {
-        for (ImageView view : views) {
-            view.setMaxHeight(height);
-        }
-    }
 }
